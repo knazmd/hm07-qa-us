a44704d23aabc85ae461650cc8950f8331acde62
knazmd <108754081+knazmd@users.noreply.github.com>
2023-05-11 10:48:32 -0500

    Update deleteHandlers.test.js

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -1,12 +1,38 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
-    try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+test('Status code should be 200', async () => {
+    let actualStatusCode;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/orders/1`, {
 			method: 'DELETE',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
 		});
+		actualStatusCode = response.status;
 	} catch (error) {
 		console.error(error);
 	}
+	expect(actualStatusCode).toBe(200);
+});
+
+
+
+test('Status body should contain true)', async () => {
+    let actualResponseBody;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/orders/1`, {
+			method: 'DELETE',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+		actualResponseBody = await response.json();
+	} catch (error) {
+		console.error(error);
+	}
+	expect(actualResponseBody["ok"]).toBe("true");
 });

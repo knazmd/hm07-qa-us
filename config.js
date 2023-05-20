fbda13f86aaaab1b713a612860b05cb210483ebe
knazmd <108754081+knazmd@users.noreply.github.com>
2023-05-19 22:51:42 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://ebc4483a-a5ed-4689-8c03-96301cd9d452.serverhub.practicum-services.com"
+    API_URL: "https://85c132b0-8fa6-4593-8fb1-c8bad70cf5a9.serverhub.practicum-services.com"
 }
